5bd66dec394e9a94c29e89a531373c4fabc3bad8
saisarveshnaik <saisarvesh.naik007@gmail.com>
2025-01-17 17:43:27 +0530

    minor ui changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import '../styles/Header.css';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap CSS
@@ -25,40 +25,52 @@ const Header: React.FC = () => {
       window.removeEventListener('scroll', handleScroll);
     };
   }, []);
-  
-  return (
 
+  // Scroll to a section smoothly
+  const scrollToSection = (sectionId: string) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+  return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-navbar">
-    <a className="navbar-brand" href="index.php">
-      <img src="/images/logo/unityfund_logo.png" className="navbar-logo" />
-    </a>
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-  
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav mr-auto ml-auto">
-        <li className="nav-item">
-          <a className="nav-link" href="#">HOME</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#Why">WHY UNITYFUND</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#Features">FEATURES</a>
-        </li>
-      </ul>
-      <form className="form-inline my-2 my-lg-0">
-        <Link to="/login" style={{ textDecoration: 'none' }}>
-        <button className="join-btn mr-2">LOGIN</button>
-        </Link>
-        <Link to="/register" style={{ textDecoration: 'none' }}>
-        <button className="join-btn">REGISTER</button>
-        </Link>
-      </form>
-    </div>
-  </nav>
-    
+      <a className="navbar-brand" href="index.php">
+        <img src="/images/logo/unityfund_logo.png" className="navbar-logo" />
+      </a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto ml-auto">
+          <li className="nav-item">
+            <button className="nav-link" onClick={() => scrollToSection('Main')}>
+              HOME
+            </button>
+          </li>
+          <li className="nav-item">
+            <button className="nav-link" onClick={() => scrollToSection('Why')}>
+              WHY UNITYFUND
+            </button>
+          </li>
+          <li className="nav-item">
+            <button className="nav-link" onClick={() => scrollToSection('Features')}>
+              FEATURES
+            </button>
+          </li>
+        </ul>
+        <form className="form-inline my-2 my-lg-0">
+          <Link to="/login" style={{ textDecoration: 'none' }}>
+            <button className="join-btn mr-2">LOGIN</button>
+          </Link>
+          <Link to="/register" style={{ textDecoration: 'none' }}>
+            <button className="join-btn">REGISTER</button>
+          </Link>
+        </form>
+      </div>
+    </nav>
   );
 };
 
